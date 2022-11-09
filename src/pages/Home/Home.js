599c51a3cd41145d9be82fd391a977055604edd9
hossain-udoy <udoyh305@gmail.com>
2022-11-09 18:31:01 +0600

    Why  us section added in the home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReviewSlider from "./AnotherSlider/AnotherSlider";
 import Banner from "./Banner/Banner";
 import LimitedService from "./LimitedService/LimitedService";
+import WhyUs from "./Why-Us/WhyUs";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Banner></Banner>
       <LimitedService></LimitedService>
       <ReviewSlider></ReviewSlider>
+      <WhyUs></WhyUs>
     </div>
   );
 };
---
+++ src/pages/Home/Why-Us/WhyUs.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { FaUserSecret, FaUserGraduate, FaUserPlus } from "react-icons/fa";
+import { CiFaceSmile } from "react-icons/ci";
+
+const WhyUs = () => {
+  return (
+    <div className="text-center py-5">
+      <h5 className="text-slate-300 font-semibold text-3xl">Why Us</h5>
+      <h2>Why You Are Travel With Easy-Travel</h2>
+
+      <div className="flex gap-5 mb-8 mx-5 mt-5">
+        <a
+          className="flex flex-col p-16 text-3xl rounded-xl border border-gray-800 bg-white text-slate-500 shadow-xl items-center justify-center shadow-slate-500"
+          href="/"
+        >
+          <FaUserSecret></FaUserSecret>
+          <h3 className="mt-3 text-xl font-bold text-slate-900">
+            3000+ Our worldwide guide
+          </h3>
+        </a>
+        <a
+          className="flex flex-col p-16 text-3xl rounded-xl border border-gray-800  bg-white text-slate-500  shadow-xl items-center justify-center shadow-slate-500"
+          href="/"
+        >
+          <FaUserGraduate></FaUserGraduate>
+          <h3 className="mt-3 text-xl font-bold text-slate-900">
+            Trusted travel agency
+          </h3>
+        </a>
+        <a
+          className="flex flex-col text-3xl rounded-xl border border-gray-800 bg-white text-slate-500 p-16 shadow-xl items-center justify-center shadow-slate-500"
+          href="/"
+        >
+          <FaUserPlus></FaUserPlus>
+          <h3 className="mt-3 text-xl font-bold text-slate-900">
+            10+ year of travel experience
+          </h3>
+        </a>
+        <a
+          className=" flex flex-col text-3xl rounded-xl border border-gray-800 bg-white text-slate-500 p-16 items-center justify-center shadow-xl shadow-slate-500"
+          href="/"
+        >
+          <CiFaceSmile></CiFaceSmile>
+          <h3 className="mt-3 text-xl font-bold text-slate-900">
+            90% of our traveller happy
+          </h3>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default WhyUs;
