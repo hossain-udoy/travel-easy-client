520b12b283c0b178832bb9e9445b2b5c74bbcdbe
hossain-udoy <udoyh305@gmail.com>
2022-11-10 07:13:03 +0600

    BannerItem is responsive now

--- src/pages/Home/Banner/BannerItem.js
+++ src/pages/Home/Banner/BannerItem.js
@@ -10,25 +10,26 @@ const BannerItem = ({ slide }) => {
       <img
         src={image}
         alt=""
-        className="h-[600px] overflow-hidden w-full rounded-xl"
+        className="h-[600px] overflow-hidden w-[100vw] rounded-xl"
       />
-      <div className="absolute flex justify-end transform -translate-y-1/2 left-96 top-1/4">
-        <h1 className="text-6xl font-bold text-white">Get Your Tour From Us</h1>
+      <div className="absolute transform -translate-y-1/2 left-1/4 lg:left-1/3 top-1/4">
+        <h1 className="text-2xl sm:text-3xl md:text-5xl font-bold text-white">
+          Get Your Tour From Us
+        </h1>
       </div>
       <div
-        className="absolute flex justify-end transform -translate-y-1/2 w-2/5 left-80
+        className="absolute flex justify-end transform -translate-y-1/2 w-2/5  lg:left-1/4 left-1/3
       
        top-1/2"
       >
-        <p className="text-xl text-white">
-          Travel Easy is the best platform for tourism.
+        <p className="text-xl  text-white">
+          Easy-Travel is the best platform for tourism.
         </p>
       </div>
-      <div className="absolute flex justify-center transform -translate-y-1/2 w-2/5 left-96 top-3/4">
+      <div className="absolute text-center justify-center transform -translate-y-1/2 w-2/5 lg:-left-3/4 top-3/4 left-1/4">
         <Link to="/services">
           <button className="btn neon-button mr-5">Check the Services</button>
         </Link>
-        <button className="btn btn-outline btn-warning">Warning</button>
       </div>
       <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
         <a href={`#slide${prev}`} className="btn btn-circle mr-5">
